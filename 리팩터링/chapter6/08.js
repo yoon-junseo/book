397e0219ae4cc4809bb173aaffafaefa8a89a2c1
yoon-junseo <ysj6844@naver.com>
2022-03-16 12:26:31 +0900

    feat: chapter6 - 2

---
+++ 리팩터링/chapter6/08.js
@@ -0,0 +1,44 @@
+const station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
+  ],
+};
+
+class NumberRange {
+  _data;
+
+  constructor(min, max) {
+    this._data = { min, max };
+  }
+
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+
+  contains(r) {
+    return r.temp >= this.min && r.temp <= this.max;
+  }
+}
+
+const operatingPlan = {
+  temperatureFloor: 50,
+  temperatureCeiling: 56,
+};
+
+const range = new NumberRange(
+  operatingPlan.temperatureFloor,
+  operatingPlan.temperatureCeiling
+);
+
+const readingsOutsideRange = (station, range) =>
+  station.readings.filter((r) => !range.contains(r));
+
+console.log(readingsOutsideRange(station, range));
---
+++ 리팩터링/chapter6/09.js
@@ -0,0 +1,61 @@
+const baseRate = (month, year) => year - 2000 + month;
+
+class Reading {
+  _customer;
+  _quantity;
+  _month;
+  _year;
+
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity;
+  }
+  get taxableCharge() {
+    return Math.max(0, this.baseCharge - (this.year - 2000) * 0.1);
+  }
+}
+
+const acquireReading = () => ({
+  customer: "ivan",
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
+
+const client1 = () => {
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading);
+  return aReading.baseCharge;
+};
+
+const client2 = () => {
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading);
+  return aReading.taxableCharge;
+};
+
+const client3 = () => {
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading);
+  const basicChargeAmount = aReading.baseCharge;
+  return basicChargeAmount;
+};
+
+[client1, client2, client3].forEach((c) => console.log(c()));
